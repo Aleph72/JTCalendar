5a1468bb1dbef3b084f4bea798479a73c767e1de
Mike <tbyte72@gmail.com>
2018-01-05 09:51:43 +0100

    Add files via upload

---
+++ JTCalendar/Views/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  JTCalendar
+//
+//  Created by Michele Rauzino on 5/1/18.
+//
+
+#define KAmountLabelYPosition                   30.0
+#define KKmLabelYPosition                       50.0
